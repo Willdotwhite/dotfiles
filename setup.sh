b88370d4de474f86d243c87c4493d911ae73e2b5
Will White <13312237+Willdotwhite@users.noreply.github.com>
2026-07-31 11:55:53 +0100

    Terminal install setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+"$ROOT_DIR/setup/install_terminal.sh"
 "$ROOT_DIR/setup/install_software.sh"
 
 echo "Setup complete."
